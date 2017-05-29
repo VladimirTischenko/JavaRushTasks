c9bc1c6e3c467a007b1fceb3db214747675e6962
Vladimir <vtischenko@meta.ua>
2017-05-29 12:16:00 +0300

    task3502

--- 4.JavaCollections/src/com/javarush/task/test/Test.java
+++ 4.JavaCollections/src/com/javarush/task/test/Test.java
@@ -1,18 +1,26 @@
 package com.javarush.task.test;
 
-import java.lang.ref.PhantomReference;
-import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 /**
  * Created by Vladimir on 13.03.2017.
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        Map<String, Integer> map = new HashMap<>();
-//        map.put
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        System.out.println("Список до обработки дженерик-методом: " + intList);
+        Utilities.fill(intList, 0);
+        System.out.println("Список после обработки дженерик-методом: "
+                + intList);
+    }
+}
+
+class Utilities {
+    public static <T> void fill(List<T> list, T val) {
+        for (int i = 0; i < list.size(); i++)
+            list.set(i, val);
     }
 }
